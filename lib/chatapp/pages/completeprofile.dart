58f480c3d7272a939deefe93f0ad49363b4224f8
raajs <raajs@DESKTOP-KPF811P>
2022-04-23 12:43:09 +0530

    day23

--- lib/chatapp/pages/completeprofile.dart
+++ lib/chatapp/pages/completeprofile.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Completeprofile extends StatefulWidget {
   Completeprofile({Key? key}) : super(key: key);
@@ -24,13 +29,18 @@ class _CompleteprofileState extends State<Completeprofile> {
             const SizedBox(
               height: 15,
             ),
-            const CircleAvatar(
-              radius: 50,
-              child: Icon(
-                Icons.person,
-                size: 70,
-              ),
-            ),
+            CupertinoButton(
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: FileImage(imageFile!),
+                  child: const Icon(
+                    Icons.person,
+                    size: 70,
+                  ),
+                ),
+                onPressed: () {
+                  showPhotoOptions();
+                }),
             const TextField(
               decoration: InputDecoration(labelText: "Full Name"),
             ),
@@ -45,6 +55,61 @@ class _CompleteprofileState extends State<Completeprofile> {
           ],
         ),
       )),
+    );
+  }
+
+  File? imageFile;
+
+  TextEditingController fullnamecontroller = TextEditingController();
+
+  void selectImage(ImageSource local) async {
+    XFile? pickedFile = await ImagePicker().pickImage(source: local);
+
+    if (pickedFile != null) {
+      cropImages(pickedFile);
+    }
+  }
+
+  void cropImages(XFile file) async {
+    File? croppedImage = await ImageCropper().cropImage(
+        sourcePath: file.path,
+        compressQuality: 20,
+        aspectRatio: const CropAspectRatio(ratioX: 1, ratioY: 1));
+
+    if (croppedImage != null) {
+      setState(() {
+        imageFile = croppedImage;
+      });
+    }
+  }
+
+  void showPhotoOptions() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Upload Profile Picture"),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              onTap: () {
+                selectImage(ImageSource.gallery);
+                Navigator.pop(context);
+              },
+              leading: const Icon(Icons.photo_album_rounded),
+              title: const Text("Select Image from Gallary"),
+            ),
+            ListTile(
+              onTap: () {
+                selectImage(ImageSource.camera);
+                Navigator.pop(context);
+              },
+              leading: const Icon(Icons.camera_alt_rounded),
+              title: const Text("Select Image from Camera"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
